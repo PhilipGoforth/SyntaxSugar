4232cbaadd1790044e04d284e3d9414cf1ce1b70
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-10-27 07:59:09 -0500

    first save

---
+++ SyntaxSugar/SyntaxSugar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + “greater than or equal to nine”;
+            }
+
+        }
+    }
+}
