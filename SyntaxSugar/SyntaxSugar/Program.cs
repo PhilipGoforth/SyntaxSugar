6c498d1ea4330743157a5405a51702545c5ecd8b
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-10-27 08:03:15 -0500

    final

--- SyntaxSugar/SyntaxSugar/Program.cs
+++ SyntaxSugar/SyntaxSugar/Program.cs
@@ -6,18 +6,9 @@ namespace SyntaxSugar
     {
         static void Main(string[] args)
         {
-            int answer = 4;
-            string response;
-
-            if (answer < 9)
-            {
-                response = answer + " is less than nine";
-            }
-            else
-            {
-                response = answer + “greater than or equal to nine”;
-            }
-
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} greater than or equal to nine";
+            Console.WriteLine(response);
         }
     }
 }
